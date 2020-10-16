1ee903265e611831f2215bfe2a4168fd70a948df
Benjamin Shostak <benshostak@rocketmail.com>
2020-10-16 12:52:54 -0700

    more css

---
+++ Script.js
@@ -0,0 +1,9 @@
+/*submitbtn(){
+    alert("Are you ready to become organized!?");
+}
+
+var image = document.getElementById("image");
+
+image.addEventListener("mouseover", function());
+this.style ="box-shadow";
+this.width
