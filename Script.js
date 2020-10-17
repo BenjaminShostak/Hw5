ab441d4e8afc1185bdc815765c63f0c502640e18
Benjamin Shostak <benshostak@rocketmail.com>
2020-10-17 01:16:23 -0700

    added nav bar links

--- Script.js
+++ Script.js
@@ -6,4 +6,6 @@ var image = document.getElementById("image");
 
 image.addEventListener("mouseover", function());
 this.style ="box-shadow";
-this.width
+this.width*/
+<div class="cleanslate w24tz-current-time w24tz-middle" style="display: inline-block !important; visibility: hidden !important; min-width:300px !important; min-height:145px !important;"><p><a href="//24timezones.com/Anaheim/time" style="text-decoration: none" class="clock24" id="tz24-1602921410-c1783-eyJob3VydHlwZSI6MTIsInNob3dkYXRlIjoiMSIsInNob3dzZWNvbmRzIjoiMCIsImNvbnRhaW5lcl9pZCI6ImNsb2NrX2Jsb2NrX2NiNWY4YWEzYzI3OGJjZiIsInR5cGUiOiJkYiIsImxhbmciOiJlbiJ9" title="Anaheim Time" target="_blank" rel="nofollow">Current time in Anaheim</a></p><div id="clock_block_cb5f8aa3c278bcf"></div></div>
+<script type="text/javascript" src="//w.24timezones.com/l.js" async></script>
