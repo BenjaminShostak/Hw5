1a2d253197993fc2e00e7f08d3eb2f15317dc930
Benjamin Shostak <benshostak@rocketmail.com>
2020-10-18 21:54:51 -0700

    updating JS

--- Script.js
+++ Script.js
@@ -1,11 +1,23 @@
-/*submitbtn(){
-    alert("Are you ready to become organized!?");
+
+/*
+var image = document.getElementById("#submit-btn");
+
+image.addEventListener("mouseover", function(){});
+    this.style ="box-shadow:2px 2px 2px 2px black";
+    this.width = "110";
+});
+
+
+    image.addEventListener("mouseout", function(){});
+    this.style ="";
+    this.width = "100"
+});
+//
+
+alert("Are you ready to become organized!?");
+
+*/
+
+$("submitBtn").click function(){
+    $("p").append(<b>hello</b>)
 }
-
-var image = document.getElementById("image");
-
-image.addEventListener("mouseover", function());
-this.style ="box-shadow";
-this.width*/
-<div class="cleanslate w24tz-current-time w24tz-middle" style="display: inline-block !important; visibility: hidden !important; min-width:300px !important; min-height:145px !important;"><p><a href="//24timezones.com/Anaheim/time" style="text-decoration: none" class="clock24" id="tz24-1602921410-c1783-eyJob3VydHlwZSI6MTIsInNob3dkYXRlIjoiMSIsInNob3dzZWNvbmRzIjoiMCIsImNvbnRhaW5lcl9pZCI6ImNsb2NrX2Jsb2NrX2NiNWY4YWEzYzI3OGJjZiIsInR5cGUiOiJkYiIsImxhbmciOiJlbiJ9" title="Anaheim Time" target="_blank" rel="nofollow">Current time in Anaheim</a></p><div id="clock_block_cb5f8aa3c278bcf"></div></div>
-<script type="text/javascript" src="//w.24timezones.com/l.js" async></script>
